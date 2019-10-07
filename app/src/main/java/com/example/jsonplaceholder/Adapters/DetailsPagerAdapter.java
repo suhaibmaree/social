743387717f5b1094a03f7106d9fb6cc2c664e8b8
suhaib maree <suhaib.sym.m@gmail.com>
2019-10-07 11:34:22 +0300

    setup view pager fragments and adapter

---
+++ app/src/main/java/com/example/jsonplaceholder/Adapters/DetailsPagerAdapter.java
@@ -0,0 +1,59 @@
+package com.example.jsonplaceholder.Adapters;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.jsonplaceholder.views.fragments.AlbumsFragment;
+import com.example.jsonplaceholder.views.fragments.PostsFragment;
+
+public class DetailsPagerAdapter extends FragmentPagerAdapter {
+
+    private static int NUM_ITEM = 2;
+    private int id;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+
+    public DetailsPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+
+        switch (position){
+            case 0:
+                return AlbumsFragment.newInstance(0 , "Albums" , id);
+            case 1:
+                return PostsFragment.newInstance(1 , "Posts" , id);
+            default:
+                return null;
+
+
+        }
+
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_ITEM;
+    }
+
+
+    // Returns the page title for the top indicator
+    @Override
+    public CharSequence getPageTitle(int position) {
+        if (position == 0)
+            return "Albums";
+        else
+            return "Posts";
+    }
+}
---
+++ app/src/main/java/com/example/jsonplaceholder/views/fragments/AlbumsFragment.java
@@ -0,0 +1,55 @@
+package com.example.jsonplaceholder.views.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.jsonplaceholder.R;
+
+public class AlbumsFragment extends Fragment {
+
+    private int id;
+    private int page;
+    private String title;
+
+
+    public static AlbumsFragment newInstance(int page, String title, int id) {
+
+        AlbumsFragment fragment = new AlbumsFragment();
+        Bundle args = new Bundle();
+        args.putInt("page", page);
+        args.putString("title", title);
+        args.putInt("id", id);
+        fragment.setArguments(args);
+
+        return fragment;
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        page = getArguments().getInt("page");
+        id = getArguments().getInt("id" , 0);
+        title = getArguments().getString("title");
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle bundle) {
+
+        View view = inflater.inflate(R.layout.fragment_albums, container , false);
+        TextView textView =view.findViewById(R.id.album_text);
+        textView.setText("Fragment #" + page);
+        return view;
+
+    }
+}
